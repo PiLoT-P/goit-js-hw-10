1ffcf17e5bd2cc9c2e6c73cd0c158186354a4772
PiLoT-P <59173880+PiLoT-P@users.noreply.github.com>
2023-03-07 23:55:39 +0200

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,6 @@
 
 export const fetchCountries = (name) => {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`).then(response => {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(response => {
         if (!response.ok) {
             throw new Error(response.status);
         }
--- src/index.js
+++ src/index.js
@@ -5,33 +5,65 @@ import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 const inputText = document.querySelector('#search-box');
+const listCountry = document.querySelector('.country-list');
 
 function getContryName(event) {
-    // console.log(event.target.value);
     let nameContry = event.target.value.trim();
     if (!nameContry) {
+        listCountry.innerHTML = '';
         return;
     }
     console.log(nameContry)
-    fetchCountries('u').then(data => {
+    fetchCountries(nameContry).then(data => {
         console.log(data);
-        console.log(data.length);
+        let lengthRequest = data.length;
+        if (lengthRequest > 10) {
+            Notiflix.Notify.info('Cogito ergo sum');
+            return;
+        }
+        if (lengthRequest > 2 && lengthRequest < 10) {
+            printCountryMor(data);
+            return;
+        }
+        printCounryOne(data);
     }).catch(error => {
         console.log(error);
+        Notiflix.Notify.failure('Oops, there is no country with that name');
     });
-    // if (data.length > 10) {
-    //     console.log('big')
-    // }
 }
 
-inputText.addEventListener('input', debounce(getContryName,300));
-// fetchCountries('u').then(data => {
-//     console.log(data);
-//     console.log(data.length);
-// }).catch(error => {
-//     console.log(error);
-// });
+function printCountryMor(countrys) {
+    const nameCon = countrys.map((country) => {
+        return `
+            <li class = 'item'>
+                <img src="${country.flags.svg}" width ='25' height = '25'alt="">
+                <span class = 'item-text'>${country.name.official}</span>
+            </li>
+        `;
+    })
+        .join('');
+    listCountry.innerHTML = nameCon;
+    console.log('this function more');
+    console.log(nameCon);
+}
 
-// Notiflix.Notify.failure('Qui timide rogat docet negare');
+function printCounryOne(countrys) {
+    const nameCon = countrys.map((country) => {
+        const language = Object.values(country.languages);
+        return `
+            <li>
+                <img src="${country.flags.svg}" width ='25' height = '25'alt="">
+                <span class = 'item-text'>${country.name.official}</span>
+                <p>Capital:${country.capital}</p>
+                <p>Population:${country.population}</p>
+                <p>Languages:${language}</p>
+            </li>
+        `;
+    })
+        .join('');
+    listCountry.innerHTML = nameCon;
+    console.log('this function one');
+    console.log(nameCon);
+}
 
-// Notiflix.Notify.info('Cogito ergo sum');
+inputText.addEventListener('input', debounce(getContryName,DEBOUNCE_DELAY));
